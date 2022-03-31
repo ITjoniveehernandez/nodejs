c1c4db2b1d13ee0a225f79e5adbc8e44677fe3e8
ITjoniveehernandez <jonivee.hernandez@ua.edu.ph>
2022-03-31 13:52:23 +0000

    Automate Database Create using Node.js

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
 
 const app = express();
+
+function dbConnect(dbName = null) {
+    return mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password :  null,
+        database :  dbName
+    });
+}
+
+const dbCreate = async (dbName) => {
+    let connection, result;
+    try {
+        connection = await dbConnect();
+        result = await connection.execute(`CREATE DATABASE ${dbName}`);
+    } finally {
+        await connection.end();
+        return result;
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('<h1>Node.js Project</h1>');
@@ -8,5 +29,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
+    dbCreate('brands');
     console.log(`Server is listening to port ${PORT}`);
 });
